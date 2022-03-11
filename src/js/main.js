8b1cf31069f2a1ab7cbd1c523018d47d38edea76
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2022-03-11 15:04:41 +0100

    euro/usd getting data from widget

--- src/js/main.js
+++ src/js/main.js
@@ -3,33 +3,48 @@
 // service worker registration - remove if you're not going to use it
 
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
+  window.addEventListener("load", function () {
     navigator.serviceWorker.register("serviceworker.js").then(
-      function(registration) {
+      function (registration) {
         // Registration was successful
         console.log(
           "ServiceWorker registration successful with scope: ",
-          registration.scope
+          registration.scope,
         );
       },
-      function(err) {
+      function (err) {
         // registration failed :(
         console.log("ServiceWorker registration failed: ", err);
-      }
+      },
     );
   });
 }
 const exchangeRate__list = document.querySelector(".exchangeRate__list");
+setTimeout(() => {
+  let strings = [];
+
+  // const euro = document.querySelector('td[title="Euro"]');
+  const spans = document.querySelectorAll("span");
+  console.log(spans[0].innerText, spans[2].innerText, typeof spans);
+  spans.forEach((str) => strings.push(str.innerText));
+  const digits = strings.map((str) => Number(str.replace(",", ".")));
+  console.log(
+    "setTimeout ~ digits",
+    digits,
+    (digits[0] / digits[2]).toString().slice(0, 5),
+  );
+}, 700);
+
 console.log(exchangeRate__list);
 fetch("https://api.nbp.pl/api/exchangerates/tables/A")
-  .then(resp => {
+  .then((resp) => {
     return resp.json();
   })
-  .then(data => {
+  .then((data) => {
     exchangeRate__list.innerText += ` ${data[0].effectiveDate}`;
     // console.log(data[0].effectiveDate);
     // console.log(data[0].rates[7].code, data[0].rates[7].mid);
-    data[0].rates.forEach(e => {
+    data[0].rates.forEach((e) => {
       if (e.code === "EUR" || e.code === "USD") {
         console.log(`${e.code}: ${e.mid} PLN`);
         exchangeRate__list.innerHTML += `<li class='list__item'>${e.code}: ${e.mid} PLN</li>`;
