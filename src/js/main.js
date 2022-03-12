3960375936cb35a0710a460fcbb5fe8301da8139
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2022-03-12 14:02:21 +0100

    code from codesandbox copied

--- src/js/main.js
+++ src/js/main.js
@@ -20,20 +20,53 @@ if ("serviceWorker" in navigator) {
   });
 }
 const exchangeRate__list = document.querySelector(".exchangeRate__list");
-setTimeout(() => {
-  let strings = [];
+const button = document.getElementById("add_button");
+let row;
+const getData = () => {
+  let str = [];
+  let eurusd = "";
+  row = document.createElement("tr");
 
-  // const euro = document.querySelector('td[title="Euro"]');
-  const spans = document.querySelectorAll("span");
-  console.log(spans[0].innerText, spans[2].innerText, typeof spans);
-  spans.forEach((str) => strings.push(str.innerText));
-  const digits = strings.map((str) => Number(str.replace(",", ".")));
-  console.log(
-    "setTimeout ~ digits",
-    digits,
-    (digits[0] / digits[2]).toString().slice(0, 5),
-  );
-}, 700);
+  setTimeout(() => {
+    const tbody = document.querySelector("tbody");
+    console.log("tbody", tbody);
+    const widget = document.querySelector("#currenciesRates");
+    widget.addEventListener("change", () => {
+      console.log("change");
+    });
+    console.log(widget);
+    const query = document.querySelectorAll("span");
+    console.log(query);
+    query.forEach((element) => {
+      str.push(element.innerText);
+    });
+    const numbers = str.map((item) => Number(item.replace(",", ".")));
+    console.log("setTimeout ~ numbers", numbers);
+    let eurusd = (numbers[1] / numbers[3]).toString().slice(0, 5);
+    row.innerHTML = `<td>EUR/USD</td><td id="count">${eurusd}</td>`;
+    tbody.append(row);
+    const count = document.getElementById("count");
+  }, 800);
+};
+getData();
+setInterval(() => {
+  row.innerHTML = '<td>EUR/USD</td><td id="count">update</td>';
+}, 3000);
+
+// setTimeout(() => {
+// let strings = [];
+
+// const euro = document.querySelector('td[title="Euro"]');
+//   const spans = document.querySelectorAll("span");
+//   console.log(spans[0].innerText, spans[2].innerText, typeof spans);
+//   spans.forEach((str) => strings.push(str.innerText));
+//   const digits = strings.map((str) => Number(str.replace(",", ".")));
+//   console.log(
+//     "setTimeout ~ digits",
+//     digits,
+//     (digits[0] / digits[2]).toString().slice(0, 5),
+//   );
+// }, 700);
 
 console.log(exchangeRate__list);
 fetch("https://api.nbp.pl/api/exchangerates/tables/A")
